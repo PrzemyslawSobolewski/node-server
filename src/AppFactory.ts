4351dbc9dbcffe7d7cab28457fbee4230e4a17b1
PrzemyslawSobolewski <przemyslaw.sobolewski@blocklabs.eu>
2024-11-29 17:11:03 +0100

    feat: cleanup

--- src/AppFactory.ts
+++ src/AppFactory.ts
@@ -1,4 +1,3 @@
-import { elizaLogger } from '@ai16z/eliza';
 import {
   INestApplication,
   ValidationPipe,
@@ -10,7 +9,6 @@ export class AppFactory {
   static setupAppInstance(app: INestApplication) {
     this.setupVersioning(app);
     this.setupValidation(app);
-    elizaLogger.log('AppFactory.setupAppInstance()', process.env.ENV);
     if (process.env.ENV !== 'production') {
       this.setupCors(app);
     }
@@ -25,7 +23,6 @@ export class AppFactory {
   }
 
   private static setupCors(app: INestApplication) {
-    elizaLogger.log('AppFactory.setupAppInstance()', process.env.CORS_ORIGIN);
     app.enableCors({
       methods:
         process.env.CORS_METHODS || 'GET, PUT, POST, PATCH, DELETE, OPTIONS',
